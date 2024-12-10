571952385257b68fbddbe88808e037dcc946d71c
Annabel Church <215145+arc64@users.noreply.github.com>
2024-12-10 09:55:26 +0100

    feat: add project cards with JSON stub

--- app/page.js
+++ app/page.js
@@ -1,54 +1,69 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error(`HTTP error! response code: ${response.status}`);
+        }
+        console.log(response.json);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Parsed JSON:', data); // Parsed JSON data
+
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Issue fetching JSON:', error)
+      });
+
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
+      <ul className={styles.projectList}>
+        {data.map((project, index) => (
+          <li key={index} className={styles.projectCard}>
+              <img
+                      src={`/${project.image}`}
+                      alt=""
+                      className={styles.projectImage}
+              />
+              <small>{project.caption}</small>
+              <p>{project.location}</p>
+              <ul>
+                  {project.tags.map((tag) => (
+                  <li>
+                    <a href={tag.link} target="_blank" rel="noopener noreferrer">
+                      {tag.name}
+                    </a>
+                  </li>
+                  ))}
+              </ul>
+              <h2>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  {project.heading}</a>
+              </h2>
+              <p>{project.description}</p>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        ))}
+    </ul>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
+      <main className={styles.main}>
+
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://adhocracy.plus/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -61,35 +76,7 @@ export default function Home() {
           />
           Learn
         </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </main>
     </div>
   );
 }
